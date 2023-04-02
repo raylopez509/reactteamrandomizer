80b97eead1d7ed6450b2688a4e9ce389f01d7864
raylopez509 <raylopez509@gmail.com>
2023-04-02 02:13:57 -0700

    Added state hooks to App.js
    added state functions for the dropdown, which will passed when clicking on the button to determine how many teams to create

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+    const [teamQuantity, setTeamQuantity] = useState(2);
     const Title = () => {
         return <h1>Team Randomizer</h1>;
     };
@@ -11,17 +13,20 @@ function App() {
                 id="textarea"
                 name="textarea"
                 placeholder="Put list of names here separated by a new line"
-                // rows="10"
-                // cols="50"
             ></textarea>
         );
     };
 
-    const TeamQuantity = () => {
+    function TeamQuantity({ teamQuantity }) {
         return (
             <div id="teamQuantity">
                 <div>Select number of teams</div>
-                <select name="teamDropdown" id="teamDropdown">
+                <select
+                    name="teamDropdown"
+                    id="teamDropdown"
+                    value={teamQuantity}
+                    onChange={(e) => setTeamQuantity(e.target.value)}
+                >
                     <option value="2">2</option>
                     <option value="3">3</option>
                     <option value="4">4</option>
@@ -30,11 +35,15 @@ function App() {
                 </select>
             </div>
         );
-    };
+    }
 
     const TeamButton = () => {
         return (
-            <button type="button" id="btn">
+            <button
+                type="button"
+                id="btn"
+                onClick={() => getRandomTeams(teamQuantity)}
+            >
                 Get Your Teams!
             </button>
         );
@@ -43,11 +52,53 @@ function App() {
     const TeamUI = () => {
         return (
             <div id="teamUI">
-                <TeamQuantity />
+                <TeamQuantity teamQuantity={2} />
                 <TeamButton />
             </div>
         );
     };
+
+    function getRandomTeams(teamNumber) {
+        console.log("teamnumber: " + teamNumber);
+        let str = document.getElementById("textarea").value.trim();
+        console.log(str);
+        let players = str.split("\n");
+        let teamarr = [[]];
+        for (let i = 0; i < teamNumber; i++) {
+            teamarr[i] = [];
+        }
+        let randNum = Math.floor(Math.random() * players.length);
+        let playerLength = players.length;
+        let team = 0;
+        for (let i = 0; i < playerLength; i++) {
+            let removedPlayer = players.splice(randNum, 1);
+            teamarr[team].push(removedPlayer);
+            team++;
+            if (team >= teamNumber) {
+                team = 0;
+            }
+            randNum = Math.floor(Math.random() * players.length);
+        }
+        let teamstext = "";
+        let teamNum = 1;
+        for (let i = 0; i < teamNumber; i++) {
+            teamstext = teamstext + "Team " + teamNum + ": <br>";
+            let playerNum = 1;
+            for (let j = 0; j < teamarr[i].length; j++) {
+                teamstext =
+                    teamstext + playerNum + ". " + teamarr[i][j] + "<br>";
+                playerNum++;
+            }
+            if (i != teamNumber) {
+                teamstext = teamstext + "<br>";
+            }
+            // let teamId = "team" + teamNum;
+            // document.getElementById(teamId).innerHTML = teamstext;
+            console.log(teamstext);
+            teamNum++;
+            teamstext = "";
+        }
+    }
 
     return (
         <>
